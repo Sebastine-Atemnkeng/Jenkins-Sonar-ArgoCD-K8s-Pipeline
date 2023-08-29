85c8bb1d1c6c3a6a68c292e703e1bd94c3b057a9
Sebastine Atemnkeng <125059322+Sebastine-Atemnkeng@users.noreply.github.com>
2023-08-29 00:20:11 -0400

    Update StartApplication.java

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -12,8 +12,34 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "Congratulations!! You have soccessfully automated a sprint boot application using CI/CD Pipeline.");
-        model.addAttribute("msg", "This application is deployed on to Kubernetes using ArgoCD as a deployer. Other open source tools used in this project includes");
+        model.addAttribute("title", "Congratulations!! You have successfully automated a sprint boot application.");
+        
+        model.addAttribute("msg", "This project was realized with the following opensource DevOps CI/CD tools,");
+        
+        model.addAttribute("msg", "Linux distros: Amazon-linux, Ubuntu-23.04-server, and CentOs-9");
+
+        model.addAttribute("msg", "Git & GitHub");
+
+        model.addAttribute("msg", "Jenkins: Jenkinfile for the Pipleline code with the various stages and step");
+
+        model.addAttribute("msg", "Maven: For the build and compiler tool");
+
+        model.addAttribute("msg", "SonarQube & QualityGates: Static Application Security Testing, SAST and code quality check");
+
+        model.addAttribute("msg", "Docker & DokcerHub: Package Manager and Artifactory");
+
+        model.addAttribute("msg", "Bash shell script: To update the deployment the GitHub deployment repo with the latest Build Number");
+
+        model.addAttribute("msg", "Slack Integration with jenkins, to update the dev team of the current build status");
+
+        model.addAttribute("msg", "ArgoCD controller to periodically scan the GitHub Deployment repo for any new updates");
+
+        model.addAttribute("msg", "Minikube Cluster, as container orchastrator for the application");
+        
+        model.addAttribute("msg",  "SUCCESS = OPPORTUNITY MEETS PREPARATION. KEEP LEARNING.");
+     
+        return "index";
+    }
         return "index";
     }
 
