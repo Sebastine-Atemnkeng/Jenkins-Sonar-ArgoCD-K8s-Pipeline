d5208c905212baf93c5c8812fdd45fa82dcc9ab8
Sebastine Atemnkeng <125059322+Sebastine-Atemnkeng@users.noreply.github.com>
2023-08-29 03:24:38 -0400

    Update StartApplication.java

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -1,38 +1,25 @@
-package com.abhishek;
+@GetMapping("/")
+public String index(final Model model) {
+    model.addAttribute("title", "Congratulations!! You have successfully automated a Spring Boot application.");
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+    // First line of the message
+    model.addAttribute("msgFirstLine", "This project was realized with the following open-source DevOps CI/CD tools:");
 
-@SpringBootApplication
-@Controller
-public class StartApplication {
+    // List of messages
+    List<String> msgLines = Arrays.asList(
+        "- Linux distros: Amazon Linux, Ubuntu 23.04 Server, and CentOS 9",
+        "- Git & GitHub",
+        "- Jenkins: Jenkinsfile for the Pipeline code with various stages and steps",
+        "- Maven: For build and compilation",
+        "- SonarQube & QualityGates: Static Application Security Testing (SAST) and code quality check",
+        "- Docker & DockerHub: Package Manager and Artifactory",
+        "- Bash shell script: To update the deployment GitHub repo with the latest Build Number",
+        "- Slack Integration with Jenkins, to update the dev team on the current build status",
+        "- ArgoCD controller to periodically scan the GitHub Deployment repo for any new updates",
+        "- Minikube Cluster as container orchestrator for the application",
+        "SUCCESS = OPPORTUNITY MEETS PREPARATION. KEEP LEARNING."
+    );
 
-    @GetMapping("/")
-    public String index(final Model model) {
-        model.addAttribute("title", "Congratulations! You have successfully automated a Spring Boot application.");
-
-        StringBuilder msgBuilder = new StringBuilder();
-        msgBuilder.append("This project was created using various open-source DevOps CI/CD tools:\n");
-        msgBuilder.append(" - Linux distributions: Amazon Linux, Ubuntu 23.04 Server, and CentOS 9\n");
-        msgBuilder.append(" - Git & GitHub for version control\n");
-        msgBuilder.append(" - Jenkins with a Jenkinsfile for defining the pipeline stages and steps\n");
-        msgBuilder.append(" - Maven for build and compilation\n");
-        msgBuilder.append(" - SonarQube & Quality Gates for Static Application Security Testing (SAST) and code quality checks\n");
-        msgBuilder.append(" - Docker & DockerHub for packaging and artifact management\n");
-        msgBuilder.append(" - Bash shell scripting to update the GitHub deployment repository with the latest Build Number\n");
-        msgBuilder.append(" - Slack integration with Jenkins to notify the development team about the build status\n");
-        msgBuilder.append(" - ArgoCD controller for periodic scanning of the GitHub Deployment repository for updates\n");
-        msgBuilder.append(" - Minikube Cluster as a container orchestrator for the application\n\n");
-        msgBuilder.append("SUCCESS = When Opportunity Meets Preparation. Keep Learning.");
-
-        model.addAttribute("msg", msgBuilder.toString());
-        return "index";
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(StartApplication.class, args);
-    }
+    model.addAttribute("msgLines", msgLines);
+    return "index";
 }
