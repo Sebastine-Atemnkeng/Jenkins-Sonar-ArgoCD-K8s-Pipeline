d8e2842f8d8d4c5bcea9500a0d3ecbff2ab77fa0
Sebastine Atemnkeng <125059322+Sebastine-Atemnkeng@users.noreply.github.com>
2023-08-28 23:23:10 -0400

    Update StartApplication.java

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -12,8 +12,9 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
-        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
+        model.addAttribute("title", "Congratulations!! You have soccessfully automated a sprint boot application using CI/CD Pipeline.");
+        model.addAttribute("msg", "This application is deployed on to Kubernetes using ArgoCD as a deployer. Other open source tools used in this project includes");
+        model.addAttribute("Git, GitHub, Jenkins, Maven, DockerHub, ArgoCD and Kubernetes. Happy Learning DevOPs. ")
         return "index";
     }
 
