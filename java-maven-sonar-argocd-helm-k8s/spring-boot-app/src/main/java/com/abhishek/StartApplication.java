3c0f03bf4a60f65fea75a9a83cbb363b21b77baf
Sebastine Atemnkeng <125059322+Sebastine-Atemnkeng@users.noreply.github.com>
2023-08-30 09:48:34 -0400

    Update StartApplication.java

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -1,25 +1,25 @@
-package com.abhishek; // Make sure this package path matches your project structure
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@SpringBootApplication
-public class StartApplication {
+import static org.mockito.Mockito.*;
 
-    public static void main(String[] args) {
-        SpringApplication.run(StartApplication.class, args);
-    }
+public class WebControllerTest {
 
-    @Controller
-    public class WebController {
+    @Mock
+    private Model mockModel;
 
-        @GetMapping("/")
-        public String getHomePage(Model model) {
-            model.addAttribute("title", "Congratulations!! You have successfully automated a Spring Boot application.");
-            return "index"; // This should match the name of your HTML file (without the extension)
-        }
+    @Test
+    public void testGetHomePage() {
+        WebController webController = new WebController();
+
+        String result = webController.getHomePage(mockModel);
+
+        // Here, you can add assertions to verify the behavior of the method
+        // For example, you might verify that the title attribute is added to the model
+        verify(mockModel).addAttribute(eq("title"), anyString());
+
+        // You might also verify the returned value, if applicable
+        // assertEquals("index", result);
     }
 }
