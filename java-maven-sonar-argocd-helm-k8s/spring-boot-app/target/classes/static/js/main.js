eea316be993ae0cc24ca341128013c02b8bb8fbf
Sebastine-Atemnkeng <sebastine.dev@gmail.com>
2023-08-30 14:51:40 +0000

    Update deployment image to version 144

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/target/classes/static/js/main.js
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/target/classes/static/js/main.js
@@ -1,30 +1,23 @@
 $(document).ready(function(){
-    // Sticky Navbar and "Go To Top" Button
-    $(window).scroll(function(){
-        if (this.scrollY > 20) {
-            $(".navbar").addClass("sticky");
-            $(".goTop").fadeIn();
-        } else {
-            $(".navbar").removeClass("sticky");
-            $(".goTop").fadeOut();
-        }
-    });
-
-    // Scroll to Top
-    $(".goTop").click(function(){
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-    });
-
-    // Toggle Navbar Menu
-    $(".menu-toggler").click(function(){
-        $(this).toggleClass("active");
-        $(".navbar-menu").toggleClass("active");
-    });
-
-    // Magnific Popup for Image Gallery
-    $(".works").magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        gallery: { enabled: true }
-    });
+	$(window).scroll(function(){
+		if(this.scrollY > 20){
+			$(".navbar").addClass("sticky");
+			$(".goTop").fadeIn();
+		}
+		else{
+			$(".navbar").removeClass("sticky");
+			$(".goTop").fadeOut();
+			
+		}
+	});
+	$(".goTop").click(function(){scroll(0,0)});
+	$(".menu-toggler").click(function(){
+		$(this).toggleClass("active");
+		$(".navbar-menu").toggleClass("active");
+	});
+	$(".works").magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		gallery:{enabled:true}
+	});
 });
