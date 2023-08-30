d516f83a460eecec94b789d5bc9ceb59ac120771
Sebastine Atemnkeng <125059322+Sebastine-Atemnkeng@users.noreply.github.com>
2023-08-30 01:26:44 -0400

    Update StartApplication.java

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -1,11 +1,28 @@
+package com.abhishek; // Make sure this package path matches your project structure
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class WebController {
+import java.util.Arrays;
+import java.util.List;
 
-    @GetMapping("/")
-    public String getHomePage() {
-        return "index"; // This should match the name of your HTML file (without the extension)
+@SpringBootApplication
+public class StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+
+    @Controller
+    public class WebController {
+
+        @GetMapping("/")
+        public String getHomePage(Model model) {
+            model.addAttribute("title", "Congratulations!! You have successfully automated a Spring Boot application.");
+            return "index"; // This should match the name of your HTML file (without the extension)
+        }
     }
 }
