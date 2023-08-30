ed13e4a3f6dde1d61c4f38072c370cf952ed472d
Sebastine Atemnkeng <125059322+Sebastine-Atemnkeng@users.noreply.github.com>
2023-08-30 01:19:06 -0400

    Update StartApplication.java

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -1,23 +1,11 @@
-$(document).ready(function(){
-	$(window).scroll(function(){
-		if(this.scrollY > 20){
-			$(".navbar").addClass("sticky");
-			$(".goTop").fadeIn();
-		}
-		else{
-			$(".navbar").removeClass("sticky");
-			$(".goTop").fadeOut();
-			
-		}
-	});
-	$(".goTop").click(function(){scroll(0,0)});
-	$(".menu-toggler").click(function(){
-		$(this).toggleClass("active");
-		$(".navbar-menu").toggleClass("active");
-	});
-	$(".works").magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		gallery:{enabled:true}
-	});
-});
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String getHomePage() {
+        return "index"; // This should match the name of your HTML file (without the extension)
+    }
+}
